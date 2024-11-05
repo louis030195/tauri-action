77be2d74be42116b30ab3225a9e5b38b3bff70b5
FabianLars <fabianlars@fabianlars.de>
2024-11-05 18:06:39 +0100

    idk what im doin

--- src/build.ts
+++ src/build.ts
@@ -67,7 +67,7 @@ export async function buildProject(
     ['build'],
     [...tauriArgs],
     root,
-    targetInfo.platform === 'macos' ? { CI: 'true' } : undefined,
+    targetInfo.platform === 'macos' ? { CIx: 'false' } : undefined,
   );
 
   // on Linux, the app product name is converted to kebab-case and `()[]{}` will be removed
